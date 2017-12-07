8bd795ac312bbac3b89bca7c2e540401d436047f
Zhu-ff <zff383900053@qq.com>
2017-12-07 16:44:46 +0800

    看的很糟糕

---
+++ 13_AppExample/Game2048/app/src/main/java/com/imooc/game2048/config/Config.java
@@ -0,0 +1,56 @@
+package com.imooc.game2048.config;
+
+import android.app.Application;
+import android.content.SharedPreferences;
+
+/**
+ * 游戏具有配置功能，需要使用一个数据持久化方法（SharedPreferences）保存这些配置
+ * 实现一个Application类，保存配置信息，并在Manifest文件中设置为程序的Application入口
+ * 可以用来放一些全局的和一些上下文都要用到变量和方法之类
+ *
+ * 完成对游戏配置文件的读写，并给其增加默认的游戏配置值
+ */
+public class Config extends Application {
+
+    /**
+     * SP对象
+     */
+    public static SharedPreferences mSp;
+
+    /**
+     * Game Goal
+     */
+    public static int mGameGoal;
+
+    /**
+     * GameView行列数
+     */
+    public static int mGameLines;
+
+    /**
+     * Item宽高
+     */
+    public static int mItemSize;
+
+    /**
+     * 记录分数
+     */
+    public static int SCROE = 0;
+
+    public static String SP_HIGH_SCROE = "SP_HIGHSCROE";
+
+    public static String KEY_HIGH_SCROE = "KEY_HighScore";
+
+    public static String KEY_GAME_LINES = "KEY_GAMELINES";
+
+    public static String KEY_GAME_GOAL = "KEY_GameGoal";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mSp = getSharedPreferences(SP_HIGH_SCROE, 0);
+        mGameLines = mSp.getInt(KEY_GAME_LINES, 4);
+        mGameGoal = mSp.getInt(KEY_GAME_GOAL, 2048);
+        mItemSize = 0;
+    }
+}
